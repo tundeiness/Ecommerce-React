05966ba33db7557da40c4b0ccc81ed1c3115081f
tundeiness <tundeiness@yahoo.co.uk>
2020-12-21 22:09:40 +0100

    edit structure of App

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="container">
-      <h1>Shopping Cart</h1>
-      <hr />
-      <div className="row">
-        <div className="col-md-8">
-          <h1>Item component</h1>
-        </div>
-        <div className="col-md-4">
-          <p>Cart component</p>
-        </div>
+const App = () => (
+  <div className="container">
+    <h1>Shopping Cart</h1>
+    <hr />
+    <div className="row">
+      <div className="col-md-8">
+        <h1>Item component</h1>
+      </div>
+      <div className="col-md-4">
+        <p>Cart component</p>
       </div>
     </div>
-  );
-}
+  </div>
+);
 
 export default App;
