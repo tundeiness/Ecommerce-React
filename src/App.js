cac92ef87f669ac55192ccd9a4908d41047c2714
tundeiness <tundeiness@yahoo.co.uk>
2020-12-21 22:09:24 +0100

    edit structure of App

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Shopping Cart</h1>
+      <hr />
+      <div className="row">
+        <div className="col-md-8">
+          <h1>Item component</h1>
+        </div>
+        <div className="col-md-4">
+          <p>Cart component</p>
+        </div>
+      </div>
     </div>
   );
 }
